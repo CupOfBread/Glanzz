cb8ad9d4b2be304a4581792729e36356979addaf
Toby <508087823@qq.com>
2022-08-08 12:24:32 +0800

    feat: 集成axios，依赖使用cdn引入缩小项目体积

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import externalGlobals from 'rollup-plugin-external-globals'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  build: {
+    rollupOptions: {
+      external: ['vue','vueRouter','axios'],
+      plugins: [
+        externalGlobals({
+          vue: 'Vue',
+          vueRouter: "VueRouter",
+          axios: "axios",
+        }),
+      ],
+    },
+  },
 })
